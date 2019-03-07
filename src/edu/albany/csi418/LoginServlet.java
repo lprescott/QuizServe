6312bc660b65b5226dc3681dc74cb4abc7fe3325
Luke Prescott <lukep@nycap.rr.com>
2019-03-07 15:05:28 -0500

    Create LoginServlet.java

---
+++ src/edu/albany/csi418/LoginServlet.java
@@ -0,0 +1,42 @@
+package edu.albany.csi418;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Email: " + email + "</h1>");
+		writer.println("<h1>Password: " + password + "</h1>");
+
+		writer.close();
+	}
+
+}
