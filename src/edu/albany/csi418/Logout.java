bb69ece890ce4e9596667a28750decdfa0c71d78
Luke Prescott <lukep@nycap.rr.com>
2019-03-13 21:21:10 -0400

    Create Logout.java
    Logout.java clears a session object

---
+++ src/edu/albany/csi418/Logout.java
@@ -0,0 +1,62 @@
+package edu.albany.csi418;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Logout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		invalidateOldSession(request);
+		response.sendRedirect("Login.jsp");
+	}
+	
+
+    /**
+     * This function gets the current http session and invalidates if it is not null.
+     * @param request the current http session
+     */
+    public static void invalidateOldSession(HttpServletRequest request) {
+
+        //get old session
+        HttpSession oldSession = request.getSession(false);
+
+        //invalidate
+        if (oldSession != null) {
+            oldSession.invalidate();
+        }
+    }
+
+}
