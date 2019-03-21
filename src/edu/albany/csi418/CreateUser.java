4ac74ec01182c7157ff506d076c9fa68881ccc5a
Luke Prescott <luke.prescott@presport.us>
2019-03-21 19:43:33 -0400

    Update CreateUser.java
    Fixed typo

--- src/edu/albany/csi418/CreateUser.java
+++ src/edu/albany/csi418/CreateUser.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/CreateUser")
 public class CreateUser extends HttpServlet {
-    private static final long serialVersionUID = 1 L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
