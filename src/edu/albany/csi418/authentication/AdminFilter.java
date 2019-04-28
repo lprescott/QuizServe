ad5872387983e41ea64171edbdcc1a112c96a0d2
Luke Prescott <luke.prescott@presport.us>
2019-04-28 19:13:00 -0400

    Initialized required methods

--- src/edu/albany/csi418/authentication/AdminFilter.java
+++ src/edu/albany/csi418/authentication/AdminFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -27,6 +28,18 @@ public class AdminFilter implements Filter {
         } else {
         	chain.doFilter(request, response);
         }
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
